3a72aefef4328f406c1fbb15ce9afaa7d2292b3d
Aaron Staples <amstaples25@gmail.com>
2024-09-22 21:15:39 -0500

    Wordle Check Method runs perfectly. Now to tidy up and do checking.

--- StaplesUnit1Proj/Controllers/HomeController.cs
+++ StaplesUnit1Proj/Controllers/HomeController.cs
@@ -8,14 +8,37 @@ namespace StaplesUnit1Proj.Controllers
     {
         [HttpGet]
         public IActionResult Index() 
-        { 
+        {
+            var w = new Wordle();
+            
             return View(); 
         }
 
         [HttpPost]
         public IActionResult Index(Wordle w)
         {
+            String answer = "Teach";
+            w.CheckSlot(answer);
+
+            if (w.slot1status == 1) { ViewBag.slot1 = "Wrong Letter";} 
+            else if (w.slot1status == 2) { ViewBag.slot1 = "Wrong Spot";} 
+            else if (w.slot1status == 3) { ViewBag.slot1 = "Correct Letter";}
+
+            if (w.slot2status == 1) { ViewBag.slot2 = "Wrong Letter"; }
+            else if (w.slot2status == 2) { ViewBag.slot2 = "Wrong Spot"; }
+            else if (w.slot2status == 3) { ViewBag.slot2 = "Correct Letter"; }
             
+            if (w.slot3status == 1) { ViewBag.slot3 = "Wrong Letter"; }
+            else if (w.slot3status == 2) { ViewBag.slot3 = "Wrong Spot"; }
+            else if (w.slot3status == 3) { ViewBag.slot3 = "Correct Letter"; }
+
+            if (w.slot4status == 1) { ViewBag.slot4 = "Wrong Letter"; }
+            else if (w.slot4status == 2) { ViewBag.slot4 = "Wrong Spot"; }
+            else if (w.slot4status == 3) { ViewBag.slot4 = "Correct Letter"; }
+
+            if (w.slot5status == 1) { ViewBag.slot5 = "Wrong Letter"; }
+            else if (w.slot5status == 2) { ViewBag.slot5 = "Wrong Spot"; }
+            else if (w.slot5status == 3) { ViewBag.slot5 = "Correct Letter"; }
             return View();
         }
 
--- StaplesUnit1Proj/Models/Wordle.cs
+++ StaplesUnit1Proj/Models/Wordle.cs
@@ -5,22 +5,30 @@ namespace StaplesUnit1Proj.Models
     public class Wordle
     {
         [Required]
-        public string answer { get; set; } = "Teach";
-        [Required]
-        public string input { get; set; } = "";
+        public string input { get; set; }
         [Required]
         public int slot1status { get; set; } = 1; //Status checks are designed to if a letter has been matched to yet.
         [Required]
         public int slot2status { get; set; } = 1;
         [Required]
-        public int slot3status { get; set; } = 1;
+        public int slot3status { get; set; } = 1; //Matching to the input slot not the Answer key
         [Required]
         public int slot4status { get; set; } = 1;
         [Required]
         public int slot5status { get; set; } = 1; //1 for no match, 2 for matching a different slot, 3 for matching;
+        [Required]
+        public bool s1used { get; set; }
+        [Required]
+        public bool s2used { get; set; }
+        [Required]
+        public bool s3used { get; set; }
+        [Required]
+        public bool s4used {  get; set; }
+        [Required]
+        public bool s5used { get; set; }
 
 
-        public void CheckSlot(string answer, string input)
+        public void CheckSlot(string answer)
         {
             int slot = 0;
             answer = answer.ToUpper();
@@ -36,18 +44,23 @@ namespace StaplesUnit1Proj.Models
                 else if (answer.Contains(i)) //Code will not run past here if letter is matched. 
                 {
                     int index = answer.IndexOf(i);
-                    //If char is in string and not already taken
-                    if (index == 0 && slot1status == 1) { Switcher(slot, 2); }
-                    else if (index == 1 && slot2status == 1) { Switcher(slot, 2); }
-                    else if (index == 2 && slot3status == 1) { Switcher(slot, 2); }
-                    else if (index == 3 && slot4status == 1) { Switcher(slot, 2); }
-                    else if (index == 4 && slot5status == 1) { Switcher(slot, 2); }
+                    //Check that the index, status and if the slot has already been used this check before establishing a value
+                    if (index == 0 && slot1status == 1 && s1used == false) { Switcher(slot, 2); s1used = true; }
+                    else if (index == 1 && slot2status == 1 && s2used == false) { Switcher(slot, 2); s2used = true; }
+                    else if (index == 2 && slot3status == 1 && s3used == false) { Switcher(slot, 2); s3used = true; }
+                    else if (index == 3 && slot4status == 1 && s4used == false) { Switcher(slot, 2); s4used = true; }
+                    else if (index == 4 && slot5status == 1 && s5used == false) { Switcher(slot, 2); s5used = true; }
                 } 
                 else 
                 { Switcher(slot, 1); }
 
                 slot++;
             }
+            s1used = false;//Reset for the next word check
+            s2used = false;
+            s3used = false;
+            s4used = false;
+            s5used = false;
             //Check each to each 1 - 1, 2-2
             //Check each other slot IF still valid. If valid then also check if valid on receiver
             //Default to black.
