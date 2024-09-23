2b9ff0fc6c17080710fb23a347e665a7cdcb4ed9
Aaron Staples <amstaples25@gmail.com>
2024-09-22 20:04:27 -0500

    Finished(Hopefully) Worlde check function. Should run

--- StaplesUnit1Proj/Models/Wordle.cs
+++ StaplesUnit1Proj/Models/Wordle.cs
@@ -5,7 +5,7 @@ namespace StaplesUnit1Proj.Models
     public class Wordle
     {
         [Required]
-        public string answer { get; set; } = "Water";
+        public string answer { get; set; } = "Teach";
         [Required]
         public string input { get; set; } = "";
         [Required]
@@ -18,16 +18,6 @@ namespace StaplesUnit1Proj.Models
         public int slot4status { get; set; } = 1;
         [Required]
         public int slot5status { get; set; } = 1; //1 for no match, 2 for matching a different slot, 3 for matching;
-        [Required]
-        public bool slot1 { get; set; } = false; //yes no if a slot has 
-        [Required]
-        public bool slot2 { get; set; } = false;
-        [Required]
-        public bool slot3 { get; set; } = false;
-        [Required]
-        public bool slot4 { get; set; } = false;
-        [Required]
-        public bool slot5 { get; set; } = false;
 
 
         public void CheckSlot(string answer, string input)
@@ -41,14 +31,20 @@ namespace StaplesUnit1Proj.Models
                 char i = input[slot];
                 if (a.Equals(i))
                 {
-                    Switch(slot, 3); //Set matching letter to green
-                } else if (answer.Contains(i)) //Code will not run past here if letter is matched. 
-                {
-                    foreach (char c in answer)
-                    {
-                        //if
-                    }
+                    Switcher(slot, 3); //Set matching letter to green
                 }
+                else if (answer.Contains(i)) //Code will not run past here if letter is matched. 
+                {
+                    int index = answer.IndexOf(i);
+                    //If char is in string and not already taken
+                    if (index == 0 && slot1status == 1) { Switcher(slot, 2); }
+                    else if (index == 1 && slot2status == 1) { Switcher(slot, 2); }
+                    else if (index == 2 && slot3status == 1) { Switcher(slot, 2); }
+                    else if (index == 3 && slot4status == 1) { Switcher(slot, 2); }
+                    else if (index == 4 && slot5status == 1) { Switcher(slot, 2); }
+                } 
+                else 
+                { Switcher(slot, 1); }
 
                 slot++;
             }
@@ -57,7 +53,7 @@ namespace StaplesUnit1Proj.Models
             //Default to black.
             //Check First then switch validities then check yellows using validity;
         }
-        public void Switch(int slot, int value)
+        public void Switcher(int slot, int value)
         {
             if (value == 3)
             {
