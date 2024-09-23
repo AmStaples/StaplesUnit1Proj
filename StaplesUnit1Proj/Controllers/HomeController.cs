c35ce58c418507906a5ff7d11b16490949fbbf5e
Aaron Staples <amstaples25@gmail.com>
2024-09-22 22:39:10 -0500

    Almost there

--- StaplesUnit1Proj/Controllers/HomeController.cs
+++ StaplesUnit1Proj/Controllers/HomeController.cs
@@ -6,40 +6,64 @@ namespace StaplesUnit1Proj.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
+        
+        [HttpGet] 
         public IActionResult Index() 
-        {
-            var w = new Wordle();
-            
+        { 
             return View(); 
         }
 
         [HttpPost]
-        public IActionResult Index(Wordle w)
+        public IActionResult Index(Wordle w) 
         {
-            String answer = "Teach";
-            w.CheckSlot(answer);
+            ViewBag.answerInput = w.answer;
+            return View("Game" , w);
+        }
 
-            if (w.slot1status == 1) { ViewBag.slot1 = "Wrong Letter";} 
-            else if (w.slot1status == 2) { ViewBag.slot1 = "Wrong Spot";} 
-            else if (w.slot1status == 3) { ViewBag.slot1 = "Correct Letter";}
-
-            if (w.slot2status == 1) { ViewBag.slot2 = "Wrong Letter"; }
-            else if (w.slot2status == 2) { ViewBag.slot2 = "Wrong Spot"; }
-            else if (w.slot2status == 3) { ViewBag.slot2 = "Correct Letter"; }
-            
-            if (w.slot3status == 1) { ViewBag.slot3 = "Wrong Letter"; }
-            else if (w.slot3status == 2) { ViewBag.slot3 = "Wrong Spot"; }
-            else if (w.slot3status == 3) { ViewBag.slot3 = "Correct Letter"; }
-
-            if (w.slot4status == 1) { ViewBag.slot4 = "Wrong Letter"; }
-            else if (w.slot4status == 2) { ViewBag.slot4 = "Wrong Spot"; }
-            else if (w.slot4status == 3) { ViewBag.slot4 = "Correct Letter"; }
-
-            if (w.slot5status == 1) { ViewBag.slot5 = "Wrong Letter"; }
-            else if (w.slot5status == 2) { ViewBag.slot5 = "Wrong Spot"; }
-            else if (w.slot5status == 3) { ViewBag.slot5 = "Correct Letter"; }
+        [HttpGet]
+        public IActionResult Game(Wordle w)
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Game(Wordle w)
+        {
+
+            if (ModelState.IsValid)
+            {
+                
+                w.CheckSlot();
+
+                if (w.slot1status == 1) { ViewBag.slot1 = "Wrong Letter"; }
+                else if (w.slot1status == 2) { ViewBag.slot1 = "Wrong Spot"; }
+                else if (w.slot1status == 3) { ViewBag.slot1 = "Correct Letter"; }
+
+                if (w.slot2status == 1) { ViewBag.slot2 = "Wrong Letter"; }
+                else if (w.slot2status == 2) { ViewBag.slot2 = "Wrong Spot"; }
+                else if (w.slot2status == 3) { ViewBag.slot2 = "Correct Letter"; }
+
+                if (w.slot3status == 1) { ViewBag.slot3 = "Wrong Letter"; }
+                else if (w.slot3status == 2) { ViewBag.slot3 = "Wrong Spot"; }
+                else if (w.slot3status == 3) { ViewBag.slot3 = "Correct Letter"; }
+
+                if (w.slot4status == 1) { ViewBag.slot4 = "Wrong Letter"; }
+                else if (w.slot4status == 2) { ViewBag.slot4 = "Wrong Spot"; }
+                else if (w.slot4status == 3) { ViewBag.slot4 = "Correct Letter"; }
+
+                if (w.slot5status == 1) { ViewBag.slot5 = "Wrong Letter"; }
+                else if (w.slot5status == 2) { ViewBag.slot5 = "Wrong Spot"; }
+                else if (w.slot5status == 3) { ViewBag.slot5 = "Correct Letter"; }
+                return View();
+            }
+            else 
+            {
+                ViewBag.Answer = w.answer;
+                ViewBag.Input = w.input;
+                ViewBag.Valid = "Invalid entry please only use 5 leter words";
+                return View(); 
+            }
+           
         }
 
     }
--- StaplesUnit1Proj/Models/Wordle.cs
+++ StaplesUnit1Proj/Models/Wordle.cs
@@ -1,106 +1,109 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.Eventing.Reader;
-namespace StaplesUnit1Proj.Models
+namespace StaplesUnit1Proj.Models;
+
+
+public class Wordle
 {
-    public class Wordle
+    [Required]
+    [StringLength(5), MinLength(5)]
+    public string answer { get; set; }
+    [Required]
+    [StringLength(5), MinLength(5)]
+    public string input { get; set; }
+    [Required]
+    public int slot1status { get; set; } = 1; //Status checks are designed to if a letter has been matched to yet.
+    [Required]
+    public int slot2status { get; set; } = 1;
+    [Required]
+    public int slot3status { get; set; } = 1; //Matching to the input slot not the Answer key
+    [Required]
+    public int slot4status { get; set; } = 1;
+    [Required]
+    public int slot5status { get; set; } = 1; //1 for no match, 2 for matching a different slot, 3 for matching;
+    [Required]
+    public bool s1used { get; set; }
+    [Required]
+    public bool s2used { get; set; }
+    [Required]
+    public bool s3used { get; set; }
+    [Required]
+    public bool s4used {  get; set; }
+    [Required]
+    public bool s5used { get; set; }
+
+
+    public void CheckSlot()
     {
-        [Required]
-        public string input { get; set; }
-        [Required]
-        public int slot1status { get; set; } = 1; //Status checks are designed to if a letter has been matched to yet.
-        [Required]
-        public int slot2status { get; set; } = 1;
-        [Required]
-        public int slot3status { get; set; } = 1; //Matching to the input slot not the Answer key
-        [Required]
-        public int slot4status { get; set; } = 1;
-        [Required]
-        public int slot5status { get; set; } = 1; //1 for no match, 2 for matching a different slot, 3 for matching;
-        [Required]
-        public bool s1used { get; set; }
-        [Required]
-        public bool s2used { get; set; }
-        [Required]
-        public bool s3used { get; set; }
-        [Required]
-        public bool s4used {  get; set; }
-        [Required]
-        public bool s5used { get; set; }
-
-
-        public void CheckSlot(string answer)
+        int slot = 0;
+        answer = answer.ToUpper();
+        input = input.ToUpper();
+        while (slot <= 4)
         {
-            int slot = 0;
-            answer = answer.ToUpper();
-            input = input.ToUpper();
-            while (slot <= 4)
+            char a = answer[slot];
+            char i = input[slot];
+            if (a.Equals(i))
             {
-                char a = answer[slot];
-                char i = input[slot];
-                if (a.Equals(i))
-                {
-                    Switcher(slot, 3); //Set matching letter to green
-                }
-                else if (answer.Contains(i)) //Code will not run past here if letter is matched. 
-                {
-                    int index = answer.IndexOf(i);
-                    //Check that the index, status and if the slot has already been used this check before establishing a value
-                    if (index == 0 && slot1status == 1 && s1used == false) { Switcher(slot, 2); s1used = true; }
-                    else if (index == 1 && slot2status == 1 && s2used == false) { Switcher(slot, 2); s2used = true; }
-                    else if (index == 2 && slot3status == 1 && s3used == false) { Switcher(slot, 2); s3used = true; }
-                    else if (index == 3 && slot4status == 1 && s4used == false) { Switcher(slot, 2); s4used = true; }
-                    else if (index == 4 && slot5status == 1 && s5used == false) { Switcher(slot, 2); s5used = true; }
-                } 
-                else 
-                { Switcher(slot, 1); }
-
-                slot++;
+                Switcher(slot, 3); //Set matching letter to green
             }
-            s1used = false;//Reset for the next word check
-            s2used = false;
-            s3used = false;
-            s4used = false;
-            s5used = false;
-            //Check each to each 1 - 1, 2-2
-            //Check each other slot IF still valid. If valid then also check if valid on receiver
-            //Default to black.
-            //Check First then switch validities then check yellows using validity;
+            else if (answer.Contains(i)) //Code will not run past here if letter is matched. 
+            {
+                int index = answer.IndexOf(i);
+                //Check that the index, status and if the slot has already been used this check before establishing a value
+                if (index == 0 && slot1status == 1 && s1used == false) { Switcher(slot, 2); s1used = true; }
+                else if (index == 1 && slot2status == 1 && s2used == false) { Switcher(slot, 2); s2used = true; }
+                else if (index == 2 && slot3status == 1 && s3used == false) { Switcher(slot, 2); s3used = true; }
+                else if (index == 3 && slot4status == 1 && s4used == false) { Switcher(slot, 2); s4used = true; }
+                else if (index == 4 && slot5status == 1 && s5used == false) { Switcher(slot, 2); s5used = true; }
+            } 
+            else 
+            { Switcher(slot, 1); }
+
+            slot++;
         }
-        public void Switcher(int slot, int value)
+        s1used = false;//Reset for the next word check
+        s2used = false;
+        s3used = false;
+        s4used = false;
+        s5used = false;
+        //Check each to each 1 - 1, 2-2
+        //Check each other slot IF still valid. If valid then also check if valid on receiver
+        //Default to black.
+        //Check First then switch validities then check yellows using validity;
+    }
+    public void Switcher(int slot, int value)
+    {
+        if (value == 3)
         {
-            if (value == 3)
+            switch (slot)
             {
-                switch (slot)
-                {
-                    case 0: slot1status = 3; break;
-                    case 1: slot2status = 3; break;
-                    case 2: slot3status = 3; break;
-                    case 3: slot4status = 3; break;
-                    case 4: slot5status = 3; break;
-                }
-            }
-            else if (value == 2)
-            {
-                switch (slot)
-                {
-                    case 0: slot1status = 2; break;
-                    case 1: slot2status = 2; break;
-                    case 2: slot3status = 2; break;
-                    case 3: slot4status = 2; break;
-                    case 4: slot5status = 2; break;
-                }
-            }
-            else if (value == 1)
-            {
-                switch (slot)
-                {
-                    case 0: slot1status = 1; break;
-                    case 1: slot2status = 1; break;
-                    case 2: slot3status = 1; break;
-                    case 3: slot4status = 1; break;
-                    case 4: slot5status = 1; break;
-                }
+                case 0: slot1status = 3; break;
+                case 1: slot2status = 3; break;
+                case 2: slot3status = 3; break;
+                case 3: slot4status = 3; break;
+                case 4: slot5status = 3; break;
             }
         }
-    }  
-}
+        else if (value == 2)
+        {
+            switch (slot)
+            {
+                case 0: slot1status = 2; break;
+                case 1: slot2status = 2; break;
+                case 2: slot3status = 2; break;
+                case 3: slot4status = 2; break;
+                case 4: slot5status = 2; break;
+            }
+        }
+        else if (value == 1)
+        {
+            switch (slot)
+            {
+                case 0: slot1status = 1; break;
+                case 1: slot2status = 1; break;
+                case 2: slot3status = 1; break;
+                case 3: slot4status = 1; break;
+                case 4: slot5status = 1; break;
+            }
+        }
+    }
+}  
