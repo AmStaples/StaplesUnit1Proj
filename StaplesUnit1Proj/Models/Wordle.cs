eb5fac32b281ac4fe44ce97989219421cc0eded2
Aaron Staples <amstaples25@gmail.com>
2024-09-22 19:39:17 -0500

    Adjusted variables to account for starting at 0. (Whoops) Continuting to imporve on detection method

--- StaplesUnit1Proj/Models/Wordle.cs
+++ StaplesUnit1Proj/Models/Wordle.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
 namespace StaplesUnit1Proj.Models
 {
     public class Wordle
@@ -8,42 +9,89 @@ namespace StaplesUnit1Proj.Models
         [Required]
         public string input { get; set; } = "";
         [Required]
-        public bool slot1status { get; set; } = false; //Status checks are designed to if a letter has been matched to yet.
+        public int slot1status { get; set; } = 1; //Status checks are designed to if a letter has been matched to yet.
         [Required]
-        public bool slot2status { get; set; } = false;
+        public int slot2status { get; set; } = 1;
         [Required]
-        public bool slot3status { get; set; } = false;
+        public int slot3status { get; set; } = 1;
         [Required]
-        public bool slot4status { get; set; } = false;
+        public int slot4status { get; set; } = 1;
         [Required]
-        public bool slot5status { get; set; } = false;
+        public int slot5status { get; set; } = 1; //1 for no match, 2 for matching a different slot, 3 for matching;
         [Required]
-        public char slot1 { get; set; } = 'a'; //Save individual Characters if need be.
+        public bool slot1 { get; set; } = false; //yes no if a slot has 
         [Required]
-        public char slot2 { get; set; } = 'A';
+        public bool slot2 { get; set; } = false;
         [Required]
-        public char slot3 { get; set; } = 'a';
+        public bool slot3 { get; set; } = false;
         [Required]
-        public char slot4 { get; set; } = 'a';
+        public bool slot4 { get; set; } = false;
         [Required]
-        public char slot5 { get; set; } = 'a';
-        //1 for black, 2 for yellow, 3 for green;
+        public bool slot5 { get; set; } = false;
 
-        public int CheckSlot(string answer, string input, int slot)
+
+        public void CheckSlot(string answer, string input)
         {
+            int slot = 0;
             answer = answer.ToUpper();
-            char a = answer[slot];
-            char i = input[slot];
-            a = char.ToUpper(a);
-            i = char.ToUpper(i);
-            if (a.Equals(i)) {
-                return 3;
-            } else if (answer.Contains(i)) {
-                return 2;
-            } else
+            input = input.ToUpper();
+            while (slot <= 4)
             {
-                return 1;
+                char a = answer[slot];
+                char i = input[slot];
+                if (a.Equals(i))
+                {
+                    Switch(slot, 3); //Set matching letter to green
+                } else if (answer.Contains(i)) //Code will not run past here if letter is matched. 
+                {
+                    foreach (char c in answer)
+                    {
+                        //if
+                    }
+                }
+
+                slot++;
+            }
+            //Check each to each 1 - 1, 2-2
+            //Check each other slot IF still valid. If valid then also check if valid on receiver
+            //Default to black.
+            //Check First then switch validities then check yellows using validity;
+        }
+        public void Switch(int slot, int value)
+        {
+            if (value == 3)
+            {
+                switch (slot)
+                {
+                    case 0: slot1status = 3; break;
+                    case 1: slot2status = 3; break;
+                    case 2: slot3status = 3; break;
+                    case 3: slot4status = 3; break;
+                    case 4: slot5status = 3; break;
+                }
+            }
+            else if (value == 2)
+            {
+                switch (slot)
+                {
+                    case 0: slot1status = 2; break;
+                    case 1: slot2status = 2; break;
+                    case 2: slot3status = 2; break;
+                    case 3: slot4status = 2; break;
+                    case 4: slot5status = 2; break;
+                }
+            }
+            else if (value == 1)
+            {
+                switch (slot)
+                {
+                    case 0: slot1status = 1; break;
+                    case 1: slot2status = 1; break;
+                    case 2: slot3status = 1; break;
+                    case 3: slot4status = 1; break;
+                    case 4: slot5status = 1; break;
+                }
             }
         }
-    }
+    }  
 }
